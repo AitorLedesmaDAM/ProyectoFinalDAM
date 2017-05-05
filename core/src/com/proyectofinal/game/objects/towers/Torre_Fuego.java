032f1259a7a4a99d6e83d18a71a99087585592e6
AitorLedesmaDAM <aitorledesmadam@gmail.com>
2017-05-05 15:38:35 +0200

    Ya se ve el mapa

---
+++ core/src/com/proyectofinal/game/objects/towers/Torre_Fuego.java
@@ -0,0 +1,8 @@
+package com.proyectofinal.game.objects.towers;
+
+/**
+ * Created by ALUMNEDAM on 05/05/2017.
+ */
+
+public class Torre_Fuego {
+}
---
+++ core/src/com/proyectofinal/game/objects/trops/Caballero.java
@@ -0,0 +1,8 @@
+package com.proyectofinal.game.objects.trops;
+
+/**
+ * Created by ALUMNEDAM on 05/05/2017.
+ */
+
+public class Caballero {
+}
