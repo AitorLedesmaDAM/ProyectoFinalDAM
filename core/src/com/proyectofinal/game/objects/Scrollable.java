769d310b987bac335c2f704306915553a449c5d2
tamoorshahzadDAM <tamoordam@gmail.com>
2017-05-09 15:56:05 +0200

    cambios

---
+++ core/src/com/proyectofinal/game/objects/Scrollable.java
@@ -0,0 +1,27 @@
+package com.proyectofinal.game.objects;
+
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+
+/**
+ * Created by ALUMNEDAM on 05/05/2017.
+ */
+
+public class Scrollable extends Actor  {
+
+    protected Vector2 position;
+    protected float velocity;
+    protected float width;
+    protected float height;
+
+
+    public Scrollable(float x, float y, float width, float height, float velocity) {
+        position = new Vector2(x, y);
+        this.velocity = velocity;
+        this.width = width;
+        this.height = height;
+
+    }
+
+
+}
