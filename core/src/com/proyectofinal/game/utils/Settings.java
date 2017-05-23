73b8435a0f17bdf80adfc76ff27fd6a7b5679d22
joanavillalbadam <joanapqpifiap@gmail.com>
2017-05-24 00:45:51 +0200

    comentado

--- core/src/com/proyectofinal/game/utils/Settings.java
+++ core/src/com/proyectofinal/game/utils/Settings.java
@@ -6,19 +6,22 @@ package com.proyectofinal.game.utils;
 
 public class Settings {
 
-    // Mida del joc, s'escalarà segons la necessitat
+    //medidas mapa AtaqueScreen
     public static final int GAME_WIDTH = 1600;
     public static final int GAME_HEIGHT = 900;
 
+    //medidas mini mapa SeleccionScreen
     public static final int MINIMAPA_WIDTH = 650;
     public static final int MINIMAPA_HEIGHT = 350;
 
+    //medidas botones seleccion de tropas de SeleccionScreen y AtaqueScreen
     public static final int TROPA_SELEC_WIDTH = 250;
     public static final int TROPA_SELEC_HEIGHT = 300;
 
+    //Buscar en que clase se utilizan estas medidas/velocidad
     public static final int TROPA_WIDTH = 70;
     public static final int TROPA_HEIGHT = 100;
-
+    
     public static final float VELOCIDAD_CABALLERO = 10f;
 
     public static int pantalla = 0;
